fe3122c15fa064770c9adf172548cce1c77bbbf1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-05-26 10:55:16 -0400

    Moved questions to separate file to improve code readability

---
+++ assets/questions.js
@@ -0,0 +1,50 @@
+// create questions and other variables
+// questions from https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+const questionList = [
+    {
+        question: 'Which of the following type of variable is visible everywhere in your JavaScript code?',
+        choices: [
+            'global variable',
+            'local variable',
+            'Both of the above',
+            'None of the above'
+        ],
+        answer: 'global variable'
+    }, {
+        question: 'Which of the following function of Array object reverses the order of the elements of an array?',
+        choices: [
+            'reverse()',
+            'push()',
+            'reduce()',
+            'invert()'
+        ],
+        answer: 'reverse()'
+    }, {
+        question: 'Which of the following type of variable takes precedence over other if names are same?',
+        choices: [
+            'global variable',
+            'local variable',
+            'Both of the above',
+            'None of the above'
+        ],
+        answer: 'local variable'
+    }, {
+        question: 'Which built-in method returns the calling string value converted to lower case?',
+        choices: [
+            'changeCase(lower)',
+            'toLower()',
+            'lowerCase()',
+            'toLowerCase()'
+        ],
+        answer: 'toLowerCase()'
+    }, {
+        question: 'Which of the following function of String object is used to find a match between a regular expression and a string, and to replace the matched substring with a new substring?',
+        choices: [
+            'concat()',
+            'match()',
+            'replace()',
+            'search()'
+        ],
+        answer: 'replace()'
+    }
+]
