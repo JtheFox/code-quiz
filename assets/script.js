dd08d63be2bdaef86eb0cd73652ba3a3adfc220a
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-05-26 10:54:53 -0400

    Moved some declarations and added clearInterval to fix issues with timer

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,3 @@
-// create questions and other variables
-// questions from https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
-const questionList = [
-    {
-        question: 'Which of the following type of variable is visible everywhere in your JavaScript code?',
-        choices: [
-            'global variable',
-            'local variable',
-            'Both of the above',
-            'None of the above'
-        ],
-        answer: 'global variable'
-    }, {
-        question: 'Which of the following function of Array object reverses the order of the elements of an array?',
-        choices: [
-            'reverse()',
-            'push()',
-            'reduce()',
-            'invert()'
-        ],
-        answer: 'reverse()'
-    }, {
-        question: 'Which of the following type of variable takes precedence over other if names are same?',
-        choices: [
-            'global variable',
-            'local variable',
-            'Both of the above',
-            'None of the above'
-        ],
-        answer: 'local variable'
-    }, {
-        question: 'Which built-in method returns the calling string value converted to lower case?',
-        choices: [
-            'changeCase(lower)',
-            'toLower()',
-            'lowerCase()',
-            'toLowerCase()'
-        ],
-        answer: 'toLowerCase()'
-    }, {
-        question: 'Which of the following function of String object is used to find a match between a regular expression and a string, and to replace the matched substring with a new substring?',
-        choices: [
-            'concat()',
-            'match()',
-            'replace()',
-            'search()'
-        ],
-        answer: 'replace()'
-    }
-]
 let currQuestion, points, time;
 const timeLimit = 50;
 
@@ -55,16 +5,21 @@ const timeLimit = 50;
 takes in query paramater and boolean value representing the use of querySelectorAll
 */
 function $(selector, all = false) {
-    if (all) return document.querySelectorAll(query);
-    else return document.querySelector(query);
+    if (all) return document.querySelectorAll(selector);
+    else return document.querySelector(selector);
 }
 
-// create event listeners
-$('.quiz-start').addEventListener('click', startQuiz);
-$('#quiz-exit').addEventListener('click', finishQuiz);
-for (let li of $('li', true)) {
-    li.addEventListener('click', evalAnswer);
-}
+// create timer
+let timer = setInterval(function () {
+    time--;
+    $('#timer').innerText = time;
+    if (time < 10) {
+        $('#timer').innerText = `0${time}`;
+    }
+    if (time < 0) {
+        finishQuiz();
+    }
+}, 1000);
 
 function startQuiz() {
     console.log('Starting quiz');
@@ -72,23 +27,12 @@ function startQuiz() {
     currQuestion = points = 0;
     time = timeLimit;
     $('#timer').innerText = time;
-    // every time the quiz is opened, setInterval decreases +1 per instance
-    // setInterval(function () {
-    //     time--;
-    //     $('#timer').innerText = time;
-    //     if (time < 10) {
-    //         $('#timer').innerText = `0${time}`;
-    //     }
-    //     if (time < 0) {
-    //         finishQuiz();
-    //     }
-    // }, 1000);
-
     $('#question-total').textContent = questionList.length;
     displayQuestion(questionList[currQuestion]);
 }
 
 function finishQuiz() {
+    clearInterval(timer);
     console.log(`Quiz complete, you got ${points} points with ${time} seconds remaining`);
     $('.quiz-window').style.display = 'none';
 }
@@ -117,4 +61,11 @@ function displayQuestion(question) {
     for (let [i, li] of $('li', true).entries()) {
         li.textContent = question.choices[i];
     }
+}
+
+// create event listeners
+$('.quiz-start').addEventListener('click', startQuiz);
+$('#quiz-exit').addEventListener('click', finishQuiz);
+for (let li of $('li', true)) {
+    li.addEventListener('click', evalAnswer);
 }
